4bbb5eb870c0348dee2186b277d000a09a3d756d
hungdz369@gmail.com <hungdz369@gmail.com>
2024-07-08 23:13:33 +0700

    update rating product

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../service/store/store";
 import { getAllProducts } from "../../service/features/productSlice";
 import { Spinner } from "../Layout/Spinner";
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
+
 
 const Product: React.FC<{ text?: string | null }> = ({ text }) => {
     const dispatch = useAppDispatch();
@@ -16,6 +20,19 @@ const Product: React.FC<{ text?: string | null }> = ({ text }) => {
     const formatCurrency = (price: number): string => {
         return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
     };
+    const renderStars = (rating: number) => {
+        const fullStars = Math.floor(rating);
+        const halfStars = rating % 1 !== 0;
+        const emptyStars = 5 - fullStars - (halfStars ? 1 : 0);
+
+        return (
+            <>
+                {Array(fullStars).fill(null).map((_, index) => <span key={`full-${index}`}><StarIcon /></span>)}
+                {halfStars && <span key="half"><StarHalfIcon /></span>}
+                {Array(emptyStars).fill(null).map((_, index) => <span key={`empty-${index}`}><StarBorderIcon /></span>)}
+            </>
+        );
+    };
     return (
         <div className="grid grid-cols-4 gap-4">
             {products.map((product, index) => (
@@ -28,7 +45,15 @@ const Product: React.FC<{ text?: string | null }> = ({ text }) => {
                             <Link to={`/product/${product.id}`}>
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{product.name}</h5>
                             </Link>
-                            <p className="mb-2 text-xl font-semibold text-gray-900 dark:text-white">{formatCurrency(product?.price)}</p>
+                            <div className="flex flex-row justify-between">
+                                <p className="mb-2 text-xl font-semibold text-gray-900 dark:text-white">{formatCurrency(product?.price)}</p>
+                                <span className="title-font font-medium text-base text-gray-900">Sold: {product?.sold} </span>
+                            </div>
+                            <div className="flex items-center gap-1 mb-2 text-yellow-500">
+                                {renderStars(product?.rating || 0)}
+
+                                {/* <span className="text-gray-600 dark:text-gray-300">{product?.rating}</span> */}
+                            </div>
                         </div>
                         <Link to={`/product/${product.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Read more
